33bf3abb2433d8eb559d077171002649ab554df3
Saurabh Singhal <saurabhsinghal98@outlook.com>
2023-02-18 18:47:16 +0530

    remove sass as cdn and add it globally

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="h-screen">
-    <NavBar />
-    <UserProduct />
-    <SideBar />
-  </div>
+  <template v-if="productStore.productInfo.length">
+    <div class="h-screen">
+      <NavBar />
+      <UserProduct />
+      <SideBar />
+    </div>
+  </template>
+  <template v-else>
+    <Loader />
+  </template>
 </template>
 
 <script>
 import UserProduct from "../components/Product/UserProduct.vue";
 import NavBar from "../components/NavBar/NavBar.vue";
 import SideBar from "../components/SideBar/SideBar.vue";
+import Loader from '../components/Loader/Loader.vue'
+import { store } from "../stores/store";
 
 export default {
-  components: { UserProduct, NavBar, SideBar },
-  setup() {},
+  components: { UserProduct, NavBar, SideBar, Loader },
+  setup() {
+    const productStore = store();
+    return {
+      productStore,
+    };
+  },
 };
 </script>
