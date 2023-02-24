07559988f7bd21bda745777849a2cf317fbdff88
Saurabh Singhal <42266069+shorbh@users.noreply.github.com>
2023-02-24 12:47:52 +0530

    add dotenv config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from 'vite-plugin-pwa'
+import dotenv from 'dotenv'
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Shoppy_vue/',
